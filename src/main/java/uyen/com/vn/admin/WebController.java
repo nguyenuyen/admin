ed953db0d0072829dc01368c56a767ddd8037709
nguyenuyen <uyenuyen.02061997@gmail.com>
2019-02-18 00:18:32 +0700

    Initial commit

---
+++ src/main/java/uyen/com/vn/admin/WebController.java
@@ -0,0 +1,13 @@
+package uyen.com.vn.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping("/")
+    public String welcome() {
+        return "ManageTimeKeepingCommon";
+    }
+}
